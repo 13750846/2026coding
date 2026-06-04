4b2bc094caaade500015ebf6658a81154a3e7c95
13750846 <13750846@me.mcu.edu.tw>
2026-06-04 16:24:21 +0800

    week15

---
+++ week15/week15-4a.py
@@ -0,0 +1,16 @@
+# week15-4a.py 學習計畫 DP - Multidimensional 第4題
+# LeetCode 72. Edit Distance
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        M,N=len(word1),len(word2)
+        @cache
+        def helper(i,j): #現在要處理 word1[i] vs. word2[j]
+            if i==M and j==N: return 0
+            if i==M: return N-j
+            if j==N: return M-i
+            if word1[i]==word2[j]: return helper(i+1,j+1)
+            #ans1 = helper(i+1, j) # 跳掉 word1[i]
+            #ans2 = helper(i, j+1) # 跳掉 word2[i]
+            #ans3 = helper(i+1, j+1)
+            return min(helper(i+1,j),helper(i,j+1),helper(i+1,j+1))+1
+        return helper(0,0)
